64a7d44029af9c058bb872a1a6201638649d37ea
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-10-19 10:50:52 +0300

    updated

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+function reverse (word){
+  return word.split("").reverse().join("");
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord=reverse(word)
+  return word===reversedWord;
 }
 
 /* 
   Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+  reverse strings
+  use an if/else statement
+  if palindrome,return true
+  if not palindrome,return false
 */
 
 // You can run `node index.js` to view these console logs
